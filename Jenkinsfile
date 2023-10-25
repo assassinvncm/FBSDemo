35b2310fc37c52da39ba54b1e877a265b575a213
assassinvncm <huntercity000000@gmail.com>
2023-10-25 16:34:59 +0700

    [DuongPT] commit changes

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,7 +2,7 @@ pipeline{
     agent any
     tools{
         maven '3.9.5'
-        jdk 'JDK17'
+        jdk 'jdk-17'
     }
     stages{
         stage('Build'){
--- src/main/java/com/fbs/post/controller/PostController.java
+++ src/main/java/com/fbs/post/controller/PostController.java
@@ -10,7 +10,7 @@ public class PostController {
 
     @GetMapping(value = "/get")
     public ResponseEntity<String> getPost(@RequestParam Long id){
-        return new ResponseEntity<>("oke okesssss", HttpStatus.OK);
+        return new ResponseEntity<>("test jenkins ", HttpStatus.OK);
     }
 
 
